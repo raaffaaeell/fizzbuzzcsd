f5dafd63cbe82315caa6b78819ed53e699819554
Rafael Palliarini Urrea <rafael.palliarini@gmail.com>
2023-07-23 11:10:02 -0400

    Commit inicial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+}
+
+object fizzBuzz {
+    fun fizz(numero: Number) {
+
+    }
+
+    fun buzz(numero: Number) {
+
+    }
+
+    fun fizzBuzz(numero: Number) {
+
+    }
+}
---
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test
+
+class FizzBuzzTest {
+
+    @Test
+    fun testFizz() {
+
+    }
+
+    @Test
+    fun testBuzz() {
+
+    }
+
+    @Test
+    fun fizzBuzz() {
+
+    }
+}
